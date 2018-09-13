ce80f9fde385bd136cda67ae0761f18b6afaec20
ljchyh <ljchyh@connect.hku.hk>
2018-09-13 12:22:07 +0000

    2 commit

--- python_demo/demo1/hello.py
+++ python_demo/demo1/hello.py
@@ -3,8 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World111!"
+
+@app.route("/user/<name>")
+def user(name):
+    return "Hello," + name
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     #app.run(host='32.221.175.37', port=1111)
