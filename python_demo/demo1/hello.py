b27de3f6088e3342cb7a38c14363ec5aaf602d98
ljchyh <ljchyh@connect.hku.hk>
2018-09-13 12:50:21 +0000

    return json ok

--- python_demo/demo1/hello.py
+++ python_demo/demo1/hello.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask,jsonify
+import json
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World111!"
+    return "Hello World112!"
 
 @app.route("/user/<name>")
 def user(name):
     return "Hello," + name
+
+@app.route("/json")
+def try_json():
+    t = {
+        'a': 1,
+        'b': 2,
+        'c': [3, 4, 5]
+    }
+    return jsonify(t)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
