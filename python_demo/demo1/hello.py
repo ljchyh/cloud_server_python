4886229c27c46cb0cb8421cd21d374688bca56ee
ljchyh <ljchyh@connect.hku.hk>
2018-09-17 16:52:01 +0000

    add mysql command

--- python_demo/demo1/hello.py
+++ python_demo/demo1/hello.py
@@ -1,8 +1,36 @@
+#coding=utf-8
+import sys
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+
 from flask import Flask,jsonify
 import json
+import MySQLdb
+import datetime
+
+
+conn = MySQLdb.connect(
+        host='localhost',
+        port = 3306,
+        user = 'root',
+        passwd = '19920527',
+        db = 'test',
+        )
+
+
+cur = conn.cursor()
+#建表，仅执行一次.
+#cur.execute("create table student(id int ,name varchar(20),class varchar(30),age varchar(10))")
+#插入数据
+#cur.execute("insert into student values('2','Tom','3 year 2 class','9')")
+#寻找指定数据并更新
+#cur.execute("update student set class='3 year 1 class' where name = 'Tom'")
+cur.close()
+conn.commit()
+#conn.close()
+
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
@@ -10,7 +38,15 @@ def hello():
 
 @app.route("/user/<name>")
 def user(name):
-    return "Hello," + name
+    userid = 1 
+    timenow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cur1 = conn.cursor()
+    cur1.execute("insert into user(name,time)\
+            values('%s','%s')"%\
+            (name,timenow))
+    cur1.close()
+    conn.commit()
+    return "你好," + name +'\n' + timenow
 
 @app.route("/json")
 def try_json():
