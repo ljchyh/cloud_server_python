36d46b9b0758b7caf20aec11bf4a4fdd72834fe2
ljchyh <ljchyh@connect.hku.hk>
2018-09-19 05:06:11 +0000

    upload img_v1

--- python_demo/demo1/hello.py
+++ python_demo/demo1/hello.py
@@ -3,6 +3,7 @@ import sys
 reload(sys) 
 sys.setdefaultencoding('utf-8')
 
+import flask
 from flask import Flask,jsonify
 import json
 import MySQLdb
@@ -18,15 +19,15 @@ conn = MySQLdb.connect(
         )
 
 
-cur = conn.cursor()
+#cur = conn.cursor()
 #建表，仅执行一次.
 #cur.execute("create table student(id int ,name varchar(20),class varchar(30),age varchar(10))")
 #插入数据
 #cur.execute("insert into student values('2','Tom','3 year 2 class','9')")
 #寻找指定数据并更新
 #cur.execute("update student set class='3 year 1 class' where name = 'Tom'")
-cur.close()
-conn.commit()
+#cur.close()
+#conn.commit()
 #conn.close()
 
 
@@ -35,6 +36,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World112!"
+
+@app.route("/upload_img", methods=['GET', 'POST'])
+def upload_img():
+    if flask.request.method == 'GET':
+        return 'waiting for img'
+    else:        
+        timenow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file = flask.request.files["file"]
+        if file:
+            print 'upload img OK'
+            return '上传成功' #return message to small app
+        else:    
+            return 'post mode 失败了'
 
 @app.route("/user/<name>")
 def user(name):
